5f05cc6e8253954efdd1c407724bbdbc53b97e70
BrianBakkala <45982065+BrianBakkala@users.noreply.github.com>
2024-10-29 09:22:34 -0400

    routing tests

--- index.js
+++ index.js
@@ -6,12 +6,19 @@
 const holden = require('./holden');
 const server = require('server');
 const { get, post } = server.router;
+const { header } = server.reply;
+
+const jsonHeader = header('Content-Type', 'application/json');
 
 // Answers to any request
 server([
-    get('/', ctx => 'Hello'),
-    get('/holden', async ctx => await holden.display()),
-    get('/holden/simple', async ctx => await holden.display_simple()),
-    get('/holden/full/:dow', async ctx => await holden.display(ctx.params.dow)),
-    get('/holden/simple/:dow', async ctx => await holden.display_simple(ctx.params.dow)),
+    get('/', ctx => jsonHeader, ctx => 'Hello'),
+    get('/favicon.ico', ctx => jsonHeader, ctx => 'Hello'),
+    get('/holden', ctx => jsonHeader, async ctx => await holden.display()),
+    get('/holden/simple', ctx => jsonHeader, async ctx => await holden.display_simple()),
+    get('/holden/full/:dow', ctx => jsonHeader, async ctx => await holden.display(ctx.params.dow)),
+    get('/holden/simple/:dow', ctx => jsonHeader, async ctx => await holden.display_simple(ctx.params.dow)),
+    get('/test', ctx => jsonHeader, async ctx => await holden.display_test(true, true)),
+    get('/test/random', ctx => jsonHeader, async ctx => await holden.display_test(Math.random() < 0.5, Math.random() < 0.5)),
+    get('/test/:trash/:recycling', ctx => jsonHeader, async ctx => await holden.display_test(ctx.params.trash, ctx.params.recycling)),
 ]);
