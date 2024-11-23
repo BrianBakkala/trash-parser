d93e44505dd40a920c68c45c4c26bf665ede9267
BrianBakkala <b.bakkala@gmail.com>
2024-11-23 14:43:13 -0500

    env file

--- particle_auth.js
+++ particle_auth.js
@@ -6,7 +6,7 @@ module.exports = {
     {
         return new Promise((resolve, reject) =>
         {
-            particle.login({ username: '_______', password: '_______' }).then(
+            particle.login({ username: process.env.PARTICLE_USER, password: process.env.PARTICLE_PASSWORD }).then(
                 function (data)
                 {
                     console.log("Login to Particle API successful.");
